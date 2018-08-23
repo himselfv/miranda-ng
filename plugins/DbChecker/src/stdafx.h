68c03799a546c19df959ad0c6ee7869124c61441
himselfv <me@boku.ru>
2018-08-23 11:16:52 +0300

    Forgot to include in the last commit.

--- plugins/DbChecker/src/stdafx.h
+++ plugins/DbChecker/src/stdafx.h
@@ -33,6 +33,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_langpack.h>
 #include <win2k.h>
 
+#include "../include/m_db_checker.h"
+
 #include "resource.h"
 #include "version.h"
 
